bfce937d41af3e8d0454c31810c1cf79a541e035
meng <mmengcx@foxmail.com>
2023-08-17 23:20:45 +0800

    Version0.2 实现轮播图效果Achieve a carousel effect

--- runner/js/index.js
+++ runner/js/index.js
@@ -27,3 +27,64 @@ input.onfocus = search;
 input.onblur = function(){
     searchList.style.display = 'none';
 }
+
+//实现轮播图功能
+let prev = document.querySelector('.banner-box .prev');
+let next = document.querySelector('.banner-box .next');
+let img = document.querySelector('.banner-box>a>img'); 
+
+let imgArr = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
+let count = 0;
+
+//上一张
+prev.onclick = function(){
+    count--;
+    if(count < 0){
+        count = imgArr.length - 1;
+    }
+
+    point();
+}
+
+//下一张
+next.onclick = function(){
+    cutImg();
+}
+
+function cutImg(){
+    count++;
+    if(count > imgArr.length - 1){
+        count = 0;
+    }
+    point();
+}
+
+let timer = setInterval(cutImg,3000);
+
+let bannerBox = document.querySelector('.banner-box');
+bannerBox.onmouseover = function(){
+    clearInterval(timer);
+}
+
+bannerBox.onmouseout = function(){
+    timer = setInterval(cutImg,3000);
+}
+
+//该函数实现了让对应图片的<li>可以亮起来
+function point(){
+    for(let j = 0; j < lis.length; j++){
+        lis[j].className='';
+    }
+
+    lis[count].className = 'active';
+    img.src = './images/' + imgArr[count];
+}
+
+let lis = document.querySelectorAll('.banner-box li');
+for(let i = 0; i < lis.length; i++){
+    lis[i].onclick = function(){
+        count = i;
+        
+        point();        
+    }
+}
